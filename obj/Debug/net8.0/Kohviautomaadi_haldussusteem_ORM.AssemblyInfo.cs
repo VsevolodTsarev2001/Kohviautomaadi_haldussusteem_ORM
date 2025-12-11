1afb03198d585ef56e540cf1a8488303bfbb0b8d
Opilane TTHK <opilane@tthk.ee>
2025-12-11 17:25:32 +0200

    intial

--- obj/Debug/net8.0/Kohviautomaadi_haldussusteem_ORM.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kohviautomaadi_haldussusteem_ORM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kohviautomaadi_haldussusteem_ORM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac497c820440547c350ea27a21cefc78ef2ba5b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kohviautomaadi_haldussusteem_ORM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kohviautomaadi_haldussusteem_ORM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
